1b94c947b18c19b95e37463635d2c7c53b3c895b
CarlosElio <carloseliogonzalezapa1997@gmail.com>
2024-02-27 09:01:13 -0500

    Reto Semana 14 Finalizado

--- main.js
+++ main.js
@@ -1,103 +1,97 @@
-import './style.css'
-import axios from 'axios'
-const url="http://localhost:3000/cats";
-let cats=[];
-const contenedorPerritos=document.querySelector("#contenedorPerritos");
-const botonEliminar=document.querySelector("#boton-eliminar");
-const cancelarEliminar=document.querySelector("#cancelar-eliminar");
-const eliminarmensaje=document.querySelector("#mensaje-eliminar"); 
+  import './style.css'
+  import axios from 'axios'
+  const url="http://localhost:3000/cats";
+  let cats=[];
+  const contenedorPerritos=document.querySelector("#contenedorPerritos");
+  const botonEliminar=document.querySelector("#boton-eliminar");
+  const botoncancelarEliminar=document.querySelector("#cancelar-eliminar");
+  const eliminarmensaje=document.querySelector("#mensaje-eliminar"); 
 
 
-//Consumiendo Servicio  Get Axios
-const reaCastAxios= async ()=>{
-  try {
-    const {data}=await axios.get(url);  
-
-    cats=data;
-    printHTML(cats);
+  //Consumiendo Servicio  Get Axios
+  const readCastAxios= async ()=>{
+    try {
+      const {data}=await axios.get(url);  
+      cats=data;
+      printHTML(cats);
+    }
+    catch (error){
+      console.log(error);
+    }
   }
-  catch (error){
-    console.log(error);
-  }
-}
-window.addEventListener("DOMContentLoaded",reaCastAxios());
+  window.addEventListener("DOMContentLoaded",readCastAxios);
 
-function printHTML(cats){
-    contenedorPerritos.innerHTML = '';
-    cats.forEach(cat => {  
-        const row=document.createElement("div");
-        row.innerHTML=` <div class="item-perrito">
-        <div class="cabecera-perrito">
-            <div class="edit-perrito">
-              <img src="imagenes/editar.svg" data-id="${cat.id}">
-            </div>
-            <div class="imagen-perrito">
-              <img src=${cat.img}>
-            </div>
-            <div class="delete-perrito">
-              <img src="imagenes/eliminar.svg" data-id="${cat.id}">
-            </div>
-        </div>
-        <div class="texto-perrito">
-            <div class="nombre-perrito">
-                  Nombre ${cat.name}
-            </div>
-            <div class="datos-perrito">
-              <div class="telefono-perrito">
-                    Telefono ${cat.telefono}
+  function printHTML(cats){
+      contenedorPerritos.innerHTML = '';
+      cats.forEach(cat => {  
+          const row=document.createElement("div");
+          row.innerHTML=` <div class="item-perrito">
+          <div class="cabecera-perrito">
+              <div class="edit-perrito">
+                <img src="imagenes/editar.svg" data-id="${cat.id}">
               </div>
-            </div>
-            <div class="pais-perrito">
-                  Pais ${cat.pais} 
-            </div>
-            <div class="descripcion-perrito">
-              <p>${cat.descripcion} </p>
-            </div>
-        </div>
-    </div>
-        `;
+              <div class="imagen-perrito">
+                <img src=${cat.img}>
+              </div>
+              <div class="delete-perrito">
+                <img src="imagenes/eliminar.svg" data-id="${cat.id}">
+              </div>
+          </div>
+          <div class="texto-perrito">
+              <div class="nombre-perrito">
+                    Nombre ${cat.name}
+              </div>
+              <div class="datos-perrito">
+                <div class="telefono-perrito">
+                      Telefono ${cat.telefono}
+                </div>
+              </div>
+              <div class="pais-perrito">
+                    Pais ${cat.pais} 
+              </div>
+              <div class="descripcion-perrito">
+                <p>${cat.descripcion} </p>
+              </div>
+          </div>
+      </div>
+          `;
+        contenedorPerritos.appendChild(row);         
+        const deletePerrito = row.querySelector("#contenedorPerritos .delete-perrito");
+        deletePerrito.addEventListener("click", mostrarconfirmacion);
+      });
+  }
 
-          contenedorPerritos.appendChild(row);         
-          const deletePerrito = row.querySelector(".delete-perrito");
-          preeliminar(deletePerrito,cat.id, row);
-    });
-}
-
-
-function preeliminar(deletePerrito,id, row){
-  //console.log(deletePerrito);
-  //console.log(id);
-  //console.log(row);      
-        
-       deletePerrito.addEventListener("click", (event)=> {
-       eliminarmensaje.style.display = 'block';
-       botonEliminar.setAttribute("data-id",id);
-        mostrarConfirmacionEliminar(id, row)
-
-  });    
-} 
-
-function mostrarConfirmacionEliminar(id, row){
-  eliminarmensaje.style.display = 'block';
-
-  botonEliminar.addEventListener("click", () => eliminarCat(id, row));
-}
-
-
-async function  eliminarCat(id, row){
-  console.log(id);
-    const response = await axios.delete(`http://localhost:3000/cats/${id}`);
-    console.log(response);
-    row.remove(); 
+  //Mostrar cuadro confirmacion
+  function mostrarconfirmacion(event){
+    const id=event.target.dataset.id;
+    eliminarmensaje.style.display = "block";
+    console.log(id);
+    botonEliminar.onclick = () => deleteDog(id);
     
-  eliminarmensaje.style.display = 'none';
- 
- 
- };
+  }
 
-botonEliminar.addEventListener("click", eliminarCat);
+  //Botones Cuadro Confirmacion
+  botoncancelarEliminar.addEventListener("click", cancelarEliminar);
 
-cancelarEliminar.addEventListener("click",()=>{
-  eliminarmensaje.style.display = 'none';
 
-})
+  function  cancelarEliminar(){
+    eliminarmensaje.style.display = 'none'
+  }
+
+  async function deleteDog(id){
+    console.log(id);
+    
+    try {
+        const response = await axios.delete(`http://localhost:3000/cats/${id}`);
+        cats = cats.filter(cat => cat.id !== id);
+        printHTML(cats);
+        eliminarmensaje.style.display = 'none'
+        
+    }
+    catch(error){
+      console.error('Error al eliminar el gato:', error);
+      
+    }
+
+
+  }
